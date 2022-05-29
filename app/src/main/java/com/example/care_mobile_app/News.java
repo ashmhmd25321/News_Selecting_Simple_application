b5c5dc187bf168a6fc664bae5dd9845caf46b23a
Ashmhmd <ashfak25321@gmail.com>
2022-05-29 09:32:49 +0530

    final commit

---
+++ app/src/main/java/com/example/care_mobile_app/News.java
@@ -0,0 +1,49 @@
+package com.example.care_mobile_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class News extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    MainAdapter mainAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+        recyclerView = (RecyclerView)findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<MainModel> options =
+                new FirebaseRecyclerOptions.Builder<MainModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("local news"),MainModel.class)
+                        .build();
+
+        mainAdapter = new MainAdapter(options);
+        recyclerView.setAdapter(mainAdapter);
+
+    }
+
+    public News(int contentLayoutId) {
+        super(contentLayoutId);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
---
+++ app/src/main/java/com/example/care_mobile_app/srilanka.java
@@ -0,0 +1,14 @@
+package com.example.care_mobile_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class srilanka extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_srilanka);
+    }
+}
